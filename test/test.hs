4de9622c6a11d38d53eb741a36e917ee0602c6c1
Michael Snoyman <michael@snoyman.com>
2009-12-10 22:03:37 +0200

    Updated test.hs

--- test/test.hs
+++ test/test.hs
@@ -1,29 +1,24 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 import Text.Yaml
 import qualified Data.ByteString as B
 import Control.Monad
-import Data.Convertible
-
--- FIXME
-instance ConvertSuccess B.ByteString B.ByteString where
-    convertSuccess = id
-instance ConvertAttempt B.ByteString B.ByteString where
-    convertAttempt = return
+import Data.Convertible.Text
+import Data.Object
+import Data.Object.Text
 
 main = do
     contents <- B.readFile "input.yaml"
-    o <- decodeText contents
+    o <- decodeText' contents
     let contents2 = encodeText o
     B.writeFile "output.yaml" contents2
-    o2 <- decodeText contents2
+    o2 <- decodeText' contents2
     print o2
-{-
+
     contents3 <- B.readFile "hebrew.yaml"
     o3 <- decodeText contents3
-    print (o3 :: B.ByteString)
+    print o3
     B.writeFile "hebrew.txt" o3
 
-    o4 <- join $ decodeFile "input.yaml"
-    print (o4 :: Object)
-    encodeFile "output2.yaml" o4
--}
+instance ConvertAttempt (Object Text Text) B.ByteString where
+    convertAttempt = fromObject
