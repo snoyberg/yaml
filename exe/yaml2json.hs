616ab6cab3c8c9f653cf7a06ab90944312e3ac77
Greg Weber <greg@gregweber.info>
2013-09-08 07:31:16 -0700

    yaml2json: handle stdin

--- exe/yaml2json.hs
+++ exe/yaml2json.hs
@@ -1,18 +1,28 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-import Data.Yaml (decodeFileEither)
+
+import Data.Yaml (decodeFileEither, decodeEither')
 import System.Environment (getArgs)
 import System.Exit
 import Data.Aeson (encode, Value)
-import Prelude hiding (putStr)
+import Prelude hiding (putStr, getContents)
 import Data.ByteString.Lazy (putStr)
+import Data.ByteString (getContents)
 
+helpMessage :: IO ()
+helpMessage = print "yaml2json FILE\n  use - to indicate stdin" >> exitFailure
+
+showJSON ejson =
+    case ejson of
+       Left err -> print err >> exitFailure
+       Right (res :: Value) -> putStr (encode res) >> exitSuccess
+
+main :: IO ()
 main = do
     args <- getArgs
     case args of
-       [] -> print "yaml2json FILE" >> exitFailure
-       (f:args) -> do
-          ejson <- decodeFileEither f
-          case ejson of
-             Left err -> print err >> exitFailure
-             Right (res :: Value) -> putStr (encode res) >> exitSuccess
+       [] -> helpMessage
+       (f:a:_)  -> helpMessage
+       -- strict getContents will read in all of stdin at once
+       ("-":[]) -> getContents >>= showJSON . decodeEither'
+       (f:[])   -> decodeFileEither f >>= showJSON
        
