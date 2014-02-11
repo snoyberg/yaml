fe4af5e01be20dcf60c853aff31ffc3d3e3fcf69
Michael Snoyman <michael@snoyman.com>
2014-02-11 11:55:47 +0200

    Add missing module

---
+++ Data/Yaml/Aeson.hs
@@ -0,0 +1,7 @@
+-- | Just a re-export of @Data.Yaml@. In the future, this will be the canonical
+-- name for that module\'s contents.
+module Data.Yaml.Aeson
+    ( module Data.Yaml
+    ) where
+
+import Data.Yaml
