bd40b5948a29511065c477da88ba3b7eea147867
Matej Kollar <208115@mail.muni.cz>
2015-09-06 12:38:57 +0200

    HLint suggestions

--- Text/Libyaml.hs
+++ Text/Libyaml.hs
@@ -4,8 +4,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE PackageImports #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -48,11 +46,10 @@ import Data.Data
 
 import Control.Monad.IO.Class
 
-import Control.Exception (throwIO, Exception, finally)
+import Control.Exception (mask_, throwIO, Exception, finally)
 import Control.Applicative
 import Control.Monad.Trans.Resource
 import Data.Conduit hiding (Source, Sink, Conduit)
-import Control.Exception (mask_)
 
 data Event =
       EventStreamStart
@@ -265,20 +262,20 @@ getEvent er = do
             yanchor <- c_get_scalar_anchor er
             anchor <- if yanchor == nullPtr
                           then return Nothing
-                          else fmap Just $ peekCString yanchor
+                          else Just <$> peekCString yanchor
             return $ Just $ EventScalar bs (bsToTag tagbs) style anchor
         YamlSequenceStartEvent -> do
             yanchor <- c_get_sequence_start_anchor er
             anchor <- if yanchor == nullPtr
                           then return Nothing
-                          else fmap Just $ peekCString yanchor
+                          else Just <$> peekCString yanchor
             return $ Just $ EventSequenceStart anchor
         YamlSequenceEndEvent -> return $ Just EventSequenceEnd
         YamlMappingStartEvent -> do
             yanchor <- c_get_mapping_start_anchor er
             anchor <- if yanchor == nullPtr
                           then return Nothing
-                          else fmap Just $ peekCString yanchor
+                          else Just <$> peekCString yanchor
             return $ Just $ EventMappingStart anchor
         YamlMappingEndEvent -> return $ Just EventMappingEnd
 
@@ -603,7 +600,7 @@ encodeFile filePath =
         file <- withCString filePath $
                     \filePath' -> withCString "w" $
                     \w' -> c_fopen filePath' w'
-        if (file == nullPtr)
+        if file == nullPtr
             then throwIO $ YamlException $ "could not open file for write: " ++ filePath
             else return file
 
