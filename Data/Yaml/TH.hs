9f9a6510274598eba86c6de75cd13be679e9cff3
Mitchell Rosen <mitchellwrosen@gmail.com>
2016-10-13 23:21:42 -0400

    Add Data.Yaml.TH

---
+++ Data/Yaml/TH.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+
+module Data.Yaml.TH
+  ( -- * Decoding
+    decodeFile
+    -- * Re-exports from "Data.Yaml"
+  , Value (..)
+  , Parser
+  , Object
+  , Array
+  , object
+  , array
+  , (.=)
+  , (.:)
+  , (.:?)
+  , (.!=)
+  , FromJSON (..)
+  ) where
+
+import Data.Yaml hiding (decodeFile)
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+
+-- | Decode a @yaml@ file at compile time.
+--
+-- @
+-- {-# LANGUAGE TemplateHaskell #-}
+--
+-- config :: Config
+-- config = $$('decodeFile' "config.yaml")
+-- @
+decodeFile :: forall a. (Lift a, FromJSON a) => FilePath -> Q (TExp a)
+decodeFile path = do
+  addDependentFile path
+  runIO (decodeFileEither path) >>= \case
+    Left err -> fail (prettyPrintParseException err)
+    Right x -> TExp <$> lift (x :: a)
