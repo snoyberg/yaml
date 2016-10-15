ab14a8926304412d1ba400829389ccf8e72b80bd
Mitchell Rosen <mitchellwrosen@gmail.com>
2016-10-15 13:32:15 -0400

    Only compile Data.Yaml.TH on ghc >= 7.8.1

--- Data/Yaml/TH.hs
+++ Data/Yaml/TH.hs
@@ -23,19 +23,22 @@ import Data.Yaml hiding (decodeFile)
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 
--- | Decode a @yaml@ file at compile time.
+-- | Decode a @yaml@ file at compile time. Only available on GHC version @7.8.1@
+-- or higher.
+--
+-- @since 0.8.19.0
+--
+-- ==== __Examples__
 --
 -- @
--- {-# LANGUAGE TemplateHaskell #-}
+-- {-\# LANGUAGE TemplateHaskell \#-}
 --
 -- config :: Config
 -- config = $$('decodeFile' "config.yaml")
 -- @
---
--- @since 0.8.19.0
 decodeFile :: forall a. (Lift a, FromJSON a) => FilePath -> Q (TExp a)
 decodeFile path = do
   addDependentFile path
   runIO (decodeFileEither path) >>= \case
     Left err -> fail (prettyPrintParseException err)
-    Right x -> TExp <$> lift (x :: a)
+    Right x -> fmap TExp (lift (x :: a))
