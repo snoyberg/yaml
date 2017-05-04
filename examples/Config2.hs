84aa352d69662a201d1c21fec49f756409efa322
kanishka <nish2575@gmail.com>
2017-05-04 17:08:28 -0400

    A second config yaml parser example
    - example with a map, set of enums, and numbers

---
+++ examples/Config2.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+
+module Config2 where
+
+import           Data.Yaml (FromJSON(..), (.:))
+import qualified Data.Yaml as Y
+import           Data.ByteString (ByteString)
+import           Data.Text (Text)
+import           Data.Aeson (withObject)
+import           Data.Map (Map)
+import           Data.Set (Set)
+import           GHC.Generics (Generic)
+
+data Flag = Awesome | Okay | Hack1
+  deriving (Eq, Show, Ord, Generic)
+
+instance FromJSON Flag
+
+data Config =
+  Config {
+    txtKey :: Text
+  , intKey :: Int
+  , dblKey :: Double
+  , txtListKey :: [Text]
+  , mapIntsKey :: Map Text Int
+  , setEnumsKey :: Set Flag
+  } deriving (Eq, Show)
+
+instance FromJSON Config where
+  parseJSON =
+    withObject "Config" (\o -> 
+      Config
+        <$> o .: "txtKey"
+        <*> o .: "intKey"
+        <*> o .: "dblKey"
+        <*> o .: "txtListKey"
+        <*> o .: "mapIntsKey"
+        <*> o .: "setEnumsKey")
+
+main :: IO ()
+main = do
+  let jsonBS :: ByteString
+      jsonBS = "txtKey: txtval\n\
+               \intKey: 1\n\
+               \dblKey: 2.0\n\
+               \txtListKey:\n\
+               \  - txtval1\n\
+               \  - txtval2\n\
+               \mapIntsKey:\n\
+               \  k1: 1\n\
+               \  k2: 2\n\
+               \setEnumsKey: [Awesome, Hack1]"
+  print (Y.decodeEither jsonBS :: Either String Config)
--- examples/Main.hs
+++ examples/Main.hs
@@ -2,9 +2,12 @@ module Main where
 
 import qualified Config
 import qualified Simple
+import qualified Config2
 
 main :: IO ()
 main = do
   Simple.main
   Config.main
+  Config2.main
+
 
