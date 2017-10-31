6802d681c2c3b85578cda42e62809fdccd4cadab
Sophie Herold <sophie@hemio.de>
2017-10-31 14:08:59 +0100

    Adds encodePretty option to drop null values

--- Data/Yaml/Pretty.hs
+++ Data/Yaml/Pretty.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE CPP #-}
 -- | Prettier YAML encoding.
 --
--- Since 0.8.13
+-- @since 0.8.13
 module Data.Yaml.Pretty
     ( encodePretty
     , Config
     , getConfCompare
     , setConfCompare
+    , getConfDropNull
+    , setConfDropNull
     , defConfig
     ) where
 
@@ -29,30 +31,47 @@ import qualified Data.Vector as V
 import Data.Yaml.Builder
 
 -- |
--- Since 0.8.13
+-- @since 0.8.13
 data Config = Config
   { confCompare :: Text -> Text -> Ordering -- ^ Function used to sort keys in objects
+  , confDropNull :: Bool
   }
 
--- | The default configuration: do not sort objects.
+-- | The default configuration: do not sort objects or drop keys
 --
--- Since 0.8.13
+-- @since 0.8.13
 defConfig :: Config
-defConfig = Config mempty
+defConfig = Config mempty False
 
 -- |
--- Since 0.8.13
+-- @since 0.8.13
 getConfCompare :: Config -> Text -> Text -> Ordering
 getConfCompare = confCompare
 
--- |
--- Since 0.8.13
+-- | Sets ordering for object keys
+--
+-- @since 0.8.13
 setConfCompare :: (Text -> Text -> Ordering) -> Config -> Config
 setConfCompare cmp c = c { confCompare = cmp }
 
+-- |
+-- @since 0.8.24
+getConfDropNull :: Config -> Bool
+getConfDropNull = confDropNull
+
+-- | Drop entries with `Null` value from objects, if set to `True`
+--
+-- @since 0.8.24
+setConfDropNull :: Bool -> Config -> Config
+setConfDropNull m c = c { confDropNull = m }
+
 pretty :: Config -> Value -> YamlBuilder
 pretty cfg = go
-  where go (Object o) = mapping (sortBy (confCompare cfg `on` fst) $ HM.toList $ HM.map go o)
+  where go (Object o) = let sort = sortBy (confCompare cfg `on` fst)
+                            select
+                              | confDropNull cfg = HM.filter (/= Null)
+                              | otherwise        = id
+                        in mapping (sort $ HM.toList $ HM.map go $ select o)
         go (Array a)  = array (go <$> V.toList a)
         go Null       = null
         go (String s) = string s
@@ -61,6 +80,6 @@ pretty cfg = go
 
 -- | Configurable 'encode'.
 --
--- Since 0.8.13
+-- @since 0.8.13
 encodePretty :: ToJSON a => Config -> a -> ByteString
 encodePretty cfg = toByteString . pretty cfg . toJSON
