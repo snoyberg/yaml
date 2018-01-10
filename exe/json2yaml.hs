145b01477b346756be3f358b13ca780e922c42d2
Yuriy Syrovetskiy <cblp@cblp.su>
2018-01-10 17:45:23 +0300

    fixup! Fix -Wincomplete-uni-patterns

--- exe/json2yaml.hs
+++ exe/json2yaml.hs
@@ -9,7 +9,7 @@ main :: IO ()
 main = do
     args <- getArgs
     let (input, output) = case args ++ repeat "-" of
-            (i:o:_) -> (i, o)
+            [i, o] -> (i, o)
             _ -> error "Usage: json2yaml [in] [out]"
     mval <- fmap J.decode $
         case input of
